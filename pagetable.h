7d2b8f42a13310cda61f71b20d57d9287456b06a
Dan <jhairguz.t@gmail.com>
2019-11-06 22:37:32 -0500

    dependencies declared in pagetable.h by options.h & fault.h

--- pagetable.h
+++ pagetable.h
@@ -5,4 +5,34 @@
 #ifndef VMSIM_PAGETABLE_H
 #define VMSIM_PAGETABLE_H
 
+#include "vmsim.h"
+
+/* valores por defecto que puedan ser sobrescritos desde la línea de comandos */
+const static int pagesize = 4096;
+const static log_pagesize = 12;
+
+/* pte significa Page Table Entry, entrada de tabla de páginas */
+typedef struct _pte {
+    uint    vfn;
+    uint    pfn;
+    int     reference;
+    bool_t  valid;
+    bool_t  modified;
+    int     counter;
+} pte_t;
+
+void pagetable_init();
+
+/* Prestar atención a la Entrada de Tabla de Página (pte) para la página virtual dada.
+ * Si la página no está en memoria, se seteará la variable 'valid'==0.
+ * Si el Número de Frames Virtuales ('vfn') nunca ha sido visto antes, se creará un nuevo
+ * pte_t con el 'vfn' dado y 'valid'==0.
+ * variable 'type'
+ * */
+pte_t *pagetable_lookup_vaddr(uint vfn, ref_kind_t type);
+
+void pagetable_test();
+
+void pagetable_dump();
+
 #endif //VMSIM_PAGETABLE_H
