960e4b878644254254f472c89004beab4b14820e
Dan <jhairguz.t@gmail.com>
2019-11-08 08:53:16 -0500

    statistics functions implementation

--- stats.c
+++ stats.c
@@ -2,4 +2,62 @@
 // Created by jhair on 5/11/19.
 //
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
 #include "stats.h"
+#include "options.h"
+
+stats_t* stats;
+
+void statsOutputType(FILE *output, type_count_t outputType, const char *label);
+
+void statsInit() {
+    stats = (stats_t*)calloc(1, sizeof(stats_t));
+    assert(stats);
+
+    if (chosenOpts.outputFile) {
+        stats->output = fopen(chosenOpts.outputFile, "a+");
+
+        if (stats->output == NULL) {
+            perror("[ERROR-STATS]: No es posible abrir el archivo de salir para escribir\n");
+            abort();
+        }
+
+        else {
+            stats->output = stdout;
+        }
+    }
+}
+
+void statsOutput() {
+    FILE *output = stats->output;
+    fprintf(output, "\n\n[INFO -STATS]: PARÁMETROS DE LA SIMULACIÓN:\n\n");
+    fprintf(output, "physicalPages  => %d \n", chosenOpts.physicalPages);
+    fprintf(output, "pageSize       => %d \n", chosenOpts.pageSize);
+    fprintf(output, "inputFile      => %s \n", chosenOpts.inputFile ? chosenOpts.inputFile : "stdin");
+    fprintf(output, "faultHandler   => %s \n", chosenOpts.faultHandler->name);
+    fprintf(output, "referenceLimit => %ld\n", chosenOpts.limit);
+
+    fprintf(output, "\n\n[INFO -STATS]: RESULTADOS DE LA SIMULACIÓN:\n\n");
+
+    fprintf(output, "\t%25s | %10s | %10s | %10s | %10s\n", "Stat Type","code", "load", "store", "total");
+    statsOutputType(output, stats->references, "Memory references");
+    statsOutputType(output, stats->miss      , "Page Faults");
+    statsOutputType(output, stats->compulsory, "Compulsory Page Faults");
+    statsOutputType(output, stats->evictDirty, "(Dirty) Page Writes");
+
+    fclose(output);
+    stats->output = NULL;
+}
+
+void statsOutputType(FILE* output, type_count_t outputType, const char *label) {
+    fprintf(output, "\t%25s | %10u | %10u | %10u | %10u\n", label,
+            outputType[REF_KIND_CODE ],
+            outputType[REF_KIND_LOAD ],
+            outputType[REF_KIND_STORE],
+            outputType[REF_KIND_CODE ] +
+            outputType[REF_KIND_LOAD ] +
+            outputType[REF_KIND_STORE]);
+}
--- stats.h
+++ stats.h
@@ -5,4 +5,46 @@
 #ifndef VMSIM_STATS_H
 #define VMSIM_STATS_H
 
+#include <sys/types.h>
+#include <stdio.h>
+
+#include "vmsim.h"
+
+typedef uint count_t;
+typedef count_t type_count_t[REF_KIND_NUM];
+
+typedef struct _stats {
+    type_count_t references;
+    type_count_t miss;
+    type_count_t compulsory;
+    type_count_t evictions;
+    type_count_t evictDirty;
+    FILE *output;
+} stats_t;
+
+extern stats_t *stats;
+
+void statsInit();
+void statsOutput();
+
+static inline void statsCompulsory(ref_kind_t type) {
+    stats->compulsory[type]++;
+}
+
+static inline void statsReference(ref_kind_t type) {
+    stats->references[type]++;
+}
+
+static inline void statsMiss(ref_kind_t type) {
+    stats->miss[type]++;
+}
+
+static inline void statsEvict(ref_kind_t type) {
+    stats->evictions[type]++;
+}
+
+static inline void statsEvictDirty(ref_kind_t type) {
+    stats->evictDirty[type]++;
+}
+
 #endif //VMSIM_STATS_H
